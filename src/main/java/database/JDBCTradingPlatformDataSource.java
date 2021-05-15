679aeb51b6b248737d6acc152f99713e5361fe29
Thomas Cleal <thomas.cleal@uqconnect.edu.au>
2021-05-15 13:19:30 +1000

    Version 0 Setting up tests

--- src/main/java/database/JDBCTradingPlatformDataSource.java
+++ src/main/java/database/JDBCTradingPlatformDataSource.java
@@ -5,6 +5,9 @@ import main.java.tradingPlatform.Transaction;
 
 import java.util.Set;
 
+/**
+ * TODO
+ */
 public class JDBCTradingPlatformDataSource implements TradingPlatformDataSource{
 
     public JDBCTradingPlatformDataSource(String propsFile) {
@@ -12,7 +15,7 @@ public class JDBCTradingPlatformDataSource implements TradingPlatformDataSource{
     }
 
     /**
-     *
+     * TODO
      */
     public void prepareDatabase(){
 
@@ -42,101 +45,163 @@ public class JDBCTradingPlatformDataSource implements TradingPlatformDataSource{
         return null;
     }
 
+    /**
+     * @see TradingPlatformDataSource#addAsset(String, int) 
+     */
     @Override
     public void addAsset(String organisation, int amount) {
 
     }
 
+    /**
+     * @see TradingPlatformDataSource#deleteAsset(String, String)
+     */
     @Override
     public void deleteAsset(String organisation, String asset) {
 
     }
 
+    /**
+     * @see TradingPlatformDataSource#updateAssetAmount(String, String, int)
+     */
     @Override
     public void updateAssetAmount(String organisation, String asset, int amount) {
 
     }
 
+    /**
+     * @see TradingPlatformDataSource#getOrganisations()
+     */
     @Override
     public Set<String> getOrganisations() {
         return null;
     }
 
+    /**
+     * @see TradingPlatformDataSource
+     */
     @Override
     public String getUserOrganisation(String username) {
         return null;
     }
 
+    /**
+     * @see TradingPlatformDataSource
+     */
     @Override
     public void deleteOrganisation(String organisation) {
 
     }
 
+    /**
+     * @see TradingPlatformDataSource
+     */
     @Override
     public Set<String> getUsers(String organisation) {
         return null;
     }
 
+    /**
+     * @see TradingPlatformDataSource
+     */
     @Override
     public String getUserPassword(String username) {
         return null;
     }
 
+    /**
+     * @see TradingPlatformDataSource
+     */
     @Override
     public void addUser(String username, String password, String type, String organisation) {
 
     }
 
+    /**
+     * @see TradingPlatformDataSource
+     */
     @Override
     public void deleteUser(String username) {
 
     }
 
+    /**
+     * @see TradingPlatformDataSource
+     */
     @Override
     public void updatePassword(String username, String password) {
 
     }
 
+    /**
+     * @see TradingPlatformDataSource
+     */
     @Override
     public Order getOrder(int idx) {
         return null;
     }
 
+    /**
+     * @see TradingPlatformDataSource
+     */
     @Override
     public Set<Order> getOrders(String organisation, String Asset, int amount, int credits, boolean isBuyOrder) {
         return null;
     }
 
+    /**
+     * @see TradingPlatformDataSource
+     */
     @Override
     public Set<Order> getAllOrders(boolean isBuyOrder) {
         return null;
     }
 
+    /**
+     * @see TradingPlatformDataSource
+     */
     @Override
     public void addOrder(String organisation, String Asset, int amount, int credits, boolean isBuyOrder) {
 
     }
 
+    /**
+     * @see TradingPlatformDataSource
+     */
     @Override
     public void deleteOrder(int idx) {
 
     }
 
+    /**
+     * @see TradingPlatformDataSource
+     */
     @Override
     public void addTransaction(String buyingOrganisation, String sellingOrganisation, String asset, int amount) {
 
     }
 
+    /**
+     * @see TradingPlatformDataSource
+     */
     @Override
     public Set<Transaction> getOrderHistory(String buyingOrganisation, String sellingOrganisation, String asset) {
         return null;
     }
 
+    /**
+     * @see TradingPlatformDataSource
+     */
     @Override
     public void deleteAll() {
 
     }
-    
+
+    /**
+     * 
+     * @param password
+     * @return
+     */
     private String hashPassword(String password) {
         return null;
     }
