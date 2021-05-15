2aaaa09accffc91f21d9242aa1fa9cbb5ce05641
OwenDR <owen.read.93@hotmail.com>
2021-05-15 17:04:42 +1000

    Version 0.3
    Black box tests

--- src/test/java/database/JDBCDataSourceTest.java
+++ src/test/java/database/JDBCDataSourceTest.java
@@ -2,21 +2,44 @@ package test.java.database;
 
 import main.java.database.JDBCTradingPlatformDataSource;
 import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.xml.namespace.QName;
 
 public class JDBCDataSourceTest {
 
     private static final String propsFile = "src/test/resources/JDBCDataSourceTest.props";
     private static final JDBCTradingPlatformDataSource dataSource = new JDBCTradingPlatformDataSource(propsFile);
-
+    private static String organisationName;
+    private static int amount;
     @BeforeAll
     static void setupDatabase() {
         dataSource.prepareDatabase();
     }
 
+
+    @Test
+    public void addOrganisationData() {
+        organisationName = "google";
+        amount = 10;
+        dataSource.addOrganisation(organisationName, amount);
+        assertEquals(organisationName, amount);
+    }
+
+    @Test
+    public void getOrganisationData() {
+        dataSource.getOrganisations();
+
+    }
+
     @Test
     public void newUser() {
-        // TODO
+
     }
+
+
+
+
 
     @AfterAll
     static void resetDatabase() {
