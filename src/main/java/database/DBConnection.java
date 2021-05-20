e37c3ebe76357db360d9302363b45a7a39f7e0e1
Thomas Cleal <thomas.cleal@uqconnect.edu.au>
2021-05-20 15:01:31 +1000

    Version 0.7 Created framework for Order and Transaction classes.

--- src/main/java/database/DBConnection.java
+++ src/main/java/database/DBConnection.java
@@ -7,10 +7,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+/**
+ * TODO
+ */
 public class DBConnection {
 
     private static Connection connection = null;
 
+    /**
+     * Constructor initializes the connection to a database.
+     *
+     * @param propsFile A props file that contains a database: url, username, password and schema.
+     * @throws SQLException when the supplied props file contains props that are not able to access the database.
+     * @throws IOException when the propsFile string is not a path to a propsFile.
+     */
     public DBConnection(String propsFile) throws SQLException, IOException {
         Properties props = new Properties();
         FileInputStream in = new FileInputStream(propsFile);
@@ -27,15 +37,20 @@ public class DBConnection {
 
     }
 
-//    public static Connection getInstance(String newPropsFile) throws SQLException, IOException{
-//        if (connection == null && propsFile == newPropsFile) {
-//            new DBConnection(propsFile);
-//        }
-//        return connection;
-//    }
-
-    public Connection getConnection() {
+    public static Connection getInstance(String propsFile) throws SQLException, IOException{
+        if (connection == null) {
+            new DBConnection(propsFile);
+        }
         return connection;
     }
+
+//    /**
+//     * Retrieves the connection that is initialised in the constructor.
+//     *
+//     * @return a connection instance of the database.
+//     */
+//    public Connection getConnection() {
+//        return connection;
+//    }
 
 }
