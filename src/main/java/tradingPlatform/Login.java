87eae9da18764cdf57d842fe9e0c290031de9e45
adam-loynd@hotmail.com <Plotonic1997>
2021-05-20 20:22:16 +1000

    Version 0.8 Added in JavaDocs for Login logic

--- src/main/java/tradingPlatform/Login.java
+++ src/main/java/tradingPlatform/Login.java
@@ -1,4 +1,66 @@
 package main.java.tradingPlatform;
 
 public class Login {
+
+    private String username;
+    private String password;
+    private String organisationalUnit;
+    private Boolean isAdmin;
+    private TPUser userInformation;
+
+    /**
+     * The constructor sets the Username and Password that the current user has provided.
+     * Runs methods checkSuppliedCreds and getUserFromDB
+     * @param userName
+     * @param password
+     */
+    public Login(String  userName, String password){
+        this.username = userName;
+        this.password = password;
+        checkSuppliedCreds(); // Check the DB for the username and password
+        // if the user is found then get the user from the DB
+            getUserFromDB();
+            // Set the organisationalUnit
+            // Set the isAdmin.
+    }
+
+    /**
+     * Based off the current values of usename, Password. This look get the remaining data about
+     * the user from the database
+     *
+     * @return All the information about the user in the database (e.g. Username, Password, Organisation and
+     * if they are an admin.
+     */
+    public TPUser getUserInfo(){
+        return userInformation;
+    }
+
+    /**
+     * Functions is used to check if the user exists in the DB
+     * @return boolean of whether or not the user existed.
+     */
+    private boolean checkSuppliedCreds(){
+        // Hash the password here.
+        // Checked the DB.
+        // TODO Change this boolean to be the actual value;
+        return true;
+    }
+
+    /**
+     *
+     */
+    private void getUserFromDB(){
+
+    }
+
+
+    // TODO Need to see where this is going
+    private void openMainGUI(){
+
+    }
+
+    //Wrong username/password message  --- catch this with an SQL error
+
+
+
 }
