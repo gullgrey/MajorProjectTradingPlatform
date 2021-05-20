9c8a5e7f7c620b5d73b86793702d97baef19cfc5
Thomas Cleal <thomas.cleal@uqconnect.edu.au>
2021-05-20 23:10:45 +1000

    Version 0.8 Added frameworks for many trading platform classes.

--- src/main/java/tradingPlatform/Asset.java
+++ src/main/java/tradingPlatform/Asset.java
@@ -8,7 +8,40 @@ public class Asset {
     public Asset() {}
 
     public Asset(String asset, String amount) {
-
+        this.asset = asset;
+        this.amount = amount;
     }
 
+
+    /**
+     *
+     * @return
+     */
+    public String getAsset() {
+        return asset;
+    }
+
+    /**
+     *
+     * @param asset
+     */
+    public void setAsset(String asset) {
+        this.asset = asset;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getAmount() {
+        return amount;
+    }
+
+    /**
+     *
+     * @param amount
+     */
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
 }
--- src/main/java/tradingPlatform/StandardUser.java
+++ src/main/java/tradingPlatform/StandardUser.java
@@ -1,7 +1,11 @@
 package main.java.tradingPlatform;
 
 import java.sql.SQLException;
+import java.util.Set;
 
+/**
+ * TODO
+ */
 public class StandardUser extends TPUser {
 
     private String username;
@@ -12,19 +16,64 @@ public class StandardUser extends TPUser {
         this.organisation = organisation;
     }
 
+    /**
+     *
+     * @param asset
+     * @param amount
+     * @param credits
+     * @throws SQLException
+     */
     public void buyAsset(String asset, int amount, int credits) throws SQLException {
         //handle credits not enough here.
         //handle negative credits
     }
 
+    /**
+     *
+     * @param asset
+     * @param amount
+     * @param credits
+     * @throws SQLException
+     */
     public void sellAsset(String asset, int amount, int credits) throws SQLException {
         //handle assets not enough/available here.
         //handle negative credits
     }
 
-    public void removeOrder(int orderId) {
+    /**
+     *
+     * @param orderId
+     * @throws SQLException
+     */
+    public void removeOrder(int orderId) throws SQLException {
 
     }
 
-//    public Set<>
+    /**
+     *
+     * @return
+     * @throws SQLException
+     */
+    public Set<Asset> getAssets() throws SQLException {
+        return null;
+    }
+
+    /**
+     *
+     * @param asset
+     * @return
+     * @throws SQLException
+     */
+    public int getAssetAmount(String asset) throws SQLException{
+        return 0;
+    }
+
+    /**
+     *
+     * @return
+     * @throws SQLException
+     */
+    public int getCredits() throws SQLException {
+        return 0;
+    }
 }
